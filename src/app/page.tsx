e4e2e0d635ffc916bfae9160d9aea73757b54808
Ateto1202 <tonysu1204@gmail.com>
2025-05-18 11:49:33 +0800

    Create the react context for selecting node

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { ReactFlowProvider } from "@xyflow/react";
 import Flow from "@/views/Flow";
+import { NodeSelectionProvider } from "@/context/NodeSelectionContext";
 
 export default function Home() {
     return (
-        <ReactFlowProvider>
-            <main className="w-screen h-screen p-20">
-                <Flow />
-            </main>
-        </ReactFlowProvider>
+        <NodeSelectionProvider>
+            <ReactFlowProvider>
+                <main className="w-screen h-screen p-20">
+                    <Flow />
+                </main>
+            </ReactFlowProvider>
+        </NodeSelectionProvider>
     );
 }
