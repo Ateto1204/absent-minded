6e0712ce9acd9bdbb1d60406710e17eedee46535
Ateto1202 <tonysu1204@gmail.com>
2025-05-19 19:50:44 +0800

    Double check if user ensure to delete the project

---
+++ src/components/ProjectDialog.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { useProjectContext } from "@/context/ProjectContext";
+import { Dialog, TextField, Button, Text, Flex } from "@radix-ui/themes";
+import { useTaskContext } from "@/context/TaskContext";
+import DeleteProjectDialog from "./DeleteProjectDialog";
+
+function ProjectDialog({
+    project,
+    isActive,
+    toggleProject,
+}: {
+    project: { id: string; name: string };
+    isActive: boolean;
+    toggleProject: () => void;
+}) {
+    const { updateProject, deleteProject, loading } = useProjectContext();
+    const { deleteTask } = useTaskContext();
+    const [name, setName] = useState(project.name);
+    const handleSave = () => {
+        updateProject({ id: project.id, name });
+    };
+
+    const handleDelete = () => {
+        deleteProject(project.id);
+        deleteTask("root");
+    };
+
+    return (
+        <Dialog.Root>
+            <li
+                onClick={toggleProject}
+                className={`truncate px-2 py-1 rounded cursor-pointer flex justify-between items-center ${
+                    isActive
+                        ? "bg-blue-500 text-white font-semibold"
+                        : "hover:bg-gray-200 text-gray-800"
+                }`}
+            >
+                <span>{project.name}</span>
+                <Dialog.Trigger>
+                    <button className="ml-2 text-sm px-2 py-0.5 rounded hover:bg-gray-300 bg-white border text-black">
+                        ...
+                    </button>
+                </Dialog.Trigger>
+            </li>
+            <Dialog.Content>
+                <Dialog.Title>
+                    <Flex justify="between">
+                        <Text>Edit Project</Text>
+                        <Dialog.Close>
+                            <Text size="4" className="cursor-pointer">
+                                x
+                            </Text>
+                        </Dialog.Close>
+                    </Flex>
+                </Dialog.Title>
+                <Flex gapX="2" my="5">
+                    <Text className="relative pt-0.5">name :</Text>
+                    <TextField.Root
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Project name"
+                    />
+                </Flex>
+                <div className="mt-4 flex gap-2">
+                    <Dialog.Root>
+                        <Dialog.Trigger>
+                            <Button
+                                color="red"
+                                variant="solid"
+                                loading={loading}
+                            >
+                                Delete
+                            </Button>
+                        </Dialog.Trigger>
+                        <Dialog.Content>
+                            <Dialog.Title />
+                            <DeleteProjectTriggerButton
+                                id={project.id}
+                                handleDelete={handleDelete}
+                            />
+                        </Dialog.Content>
+                    </Dialog.Root>
+                    <Button
+                        color="blue"
+                        variant="solid"
+                        onClick={handleSave}
+                        loading={loading}
+                    >
+                        Save
+                    </Button>
+                </div>
+            </Dialog.Content>
+        </Dialog.Root>
+    );
+}
+
+const DeleteProjectTriggerButton = ({
+    id,
+    handleDelete,
+}: {
+    id: string;
+    handleDelete: () => void;
+}) => {
+    return <DeleteProjectDialog id={id} handleDelete={handleDelete} />;
+};
+
+export default ProjectDialog;
--- src/views/ProjectMenu.tsx
+++ src/views/ProjectMenu.tsx
@@ -1,90 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import ProjectDialog from "@/components/ProjectDialog";
 import { useProjectContext } from "@/context/ProjectContext";
-import { Dialog, TextField, Button, Text, Flex } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
 import { v4 as uuidv4 } from "uuid";
-import { useTaskContext } from "@/context/TaskContext";
-
-function ProjectDialog({
-    project,
-    isActive,
-    toggleProject,
-}: {
-    project: { id: string; name: string };
-    isActive: boolean;
-    toggleProject: () => void;
-}) {
-    const { updateProject, deleteProject, loading } = useProjectContext();
-    const { deleteTask } = useTaskContext();
-    const [name, setName] = useState(project.name);
-    const handleSave = () => {
-        updateProject({ id: project.id, name });
-    };
-
-    const handleDelete = () => {
-        deleteProject(project.id);
-        deleteTask("root");
-    };
-
-    return (
-        <Dialog.Root>
-            <li
-                onClick={toggleProject}
-                className={`truncate px-2 py-1 rounded cursor-pointer flex justify-between items-center ${
-                    isActive
-                        ? "bg-blue-500 text-white font-semibold"
-                        : "hover:bg-gray-200 text-gray-800"
-                }`}
-            >
-                <span>{project.name}</span>
-                <Dialog.Trigger>
-                    <button className="ml-2 text-sm px-2 py-0.5 rounded hover:bg-gray-300 bg-white border text-black">
-                        ...
-                    </button>
-                </Dialog.Trigger>
-            </li>
-            <Dialog.Content>
-                <Dialog.Title>
-                    <Flex justify="between">
-                        <Text>Edit Project</Text>
-                        <Dialog.Close>
-                            <Text size="4" className="cursor-pointer">
-                                x
-                            </Text>
-                        </Dialog.Close>
-                    </Flex>
-                </Dialog.Title>
-                <Flex gapX="2" my="5">
-                    <Text className="relative pt-0.5">name :</Text>
-                    <TextField.Root
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder="Project name"
-                    />
-                </Flex>
-                <div className="mt-4 flex gap-2">
-                    <Button
-                        color="red"
-                        variant="solid"
-                        onClick={handleDelete}
-                        loading={loading}
-                    >
-                        Delete
-                    </Button>
-                    <Button
-                        color="blue"
-                        variant="solid"
-                        onClick={handleSave}
-                        loading={loading}
-                    >
-                        Save
-                    </Button>
-                </div>
-            </Dialog.Content>
-        </Dialog.Root>
-    );
-}
 
 function ProjectMenu() {
     const { projects, addProject, toggleProject, currentProject } =
