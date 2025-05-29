9f5cbea08c2aef77c9b25285eea191212c5a5059
Ateto1202 <tonysu1204@gmail.com>
2025-05-29 13:02:07 +0800

    Enable the apply the generating result into tasks list

--- src/components/chatRoom/MessageBubble.tsx
+++ src/components/chatRoom/MessageBubble.tsx
@@ -1,4 +1,17 @@
-import { DataList } from "@radix-ui/themes";
+import { useProjectContext } from "@/context/ProjectContext";
+import { useTaskContext } from "@/context/TaskContext";
+import Task from "@/models/entities/task/Task";
+import TaskStatus from "@/models/entities/task/TaskStatus";
+import { Button, DataList, Flex, Separator } from "@radix-ui/themes";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+enum AppliedStatus {
+    Apply = "apply",
+    Applied = "applied",
+    Failed = "failed",
+    Loading = "loading",
+}
 
 const MessageBubble = ({
     text,
@@ -7,6 +20,11 @@ const MessageBubble = ({
     text: string;
     sender: "user" | "bot";
 }) => {
+    const { addTask, success, error } = useTaskContext();
+    const { currentProject, currentRoot } = useProjectContext();
+    const [appliedStatus, setAppliedStatus] = useState<AppliedStatus>(
+        AppliedStatus.Apply
+    );
     let jsonObj = null;
     if (
         sender === "bot" &&
@@ -18,6 +36,28 @@ const MessageBubble = ({
             jsonObj = JSON.parse(text);
         } catch {}
     }
+
+    const handleApply = async () => {
+        if (currentRoot.trim() === "") {
+            console.log("current root was null");
+            return;
+        }
+        const task: Task = {
+            id: uuidv4(),
+            data: jsonObj,
+            parent: currentRoot,
+            project: currentProject,
+            status: TaskStatus.Active,
+        };
+        setAppliedStatus(AppliedStatus.Loading);
+        await addTask(task);
+        if (success) {
+            setAppliedStatus(AppliedStatus.Applied);
+        } else if (error) {
+            setAppliedStatus(AppliedStatus.Failed);
+        }
+    };
+
     return (
         <div
             className={`flex items-end ${
@@ -25,9 +65,24 @@ const MessageBubble = ({
             } w-full`}
         >
             {jsonObj ? (
-                <div className="bg-zinc-900 text-zinc-100 p-4 rounded-xl shadow border border-zinc-800 max-w-[90%]">
+                <Flex
+                    direction="column"
+                    gapY="3"
+                    className="bg-zinc-900 text-zinc-100 p-4 rounded-xl shadow border border-zinc-800 max-w-[90%]"
+                >
+                    <Flex justify="end">
+                        <Button
+                            size="1"
+                            color="gray"
+                            onClick={handleApply}
+                            disabled={appliedStatus !== AppliedStatus.Apply}
+                        >
+                            {appliedStatus}
+                        </Button>
+                    </Flex>
+                    <Separator orientation="horizontal" size="4" />
                     <DataList.Root>
-                        {Object.entries(jsonObj.data).map(([k, v]) => (
+                        {Object.entries(jsonObj).map(([k, v]) => (
                             <DataList.Item key={k} className="py-1">
                                 <DataList.Label className="text-zinc-400">
                                     {k}
@@ -38,7 +93,7 @@ const MessageBubble = ({
                             </DataList.Item>
                         ))}
                     </DataList.Root>
-                </div>
+                </Flex>
             ) : (
                 <div
                     className={`px-3 py-2 rounded-2xl shadow max-w-[70%] text-sm leading-snug ${
