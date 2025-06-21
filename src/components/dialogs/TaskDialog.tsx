1908051948d0259721be8a70a1fed4b4206e59ef
Ateto1202 <tonysu1204@gmail.com>
2025-06-21 18:21:03 +0800

    Handle the logic bug to check if task deadline was earlier than start time

--- src/components/dialogs/TaskDialog.tsx
+++ src/components/dialogs/TaskDialog.tsx
@@ -28,6 +28,10 @@ const TaskDialog = ({ id, data }: { id: string; data: TaskData }) => {
     );
 
     const handleSave = () => {
+        if (deadline && new Date(start) > new Date(deadline)) {
+            alert("The start time cannot be later than the end time");
+            return;
+        }
         const taskData = {
             label,
             description,
@@ -50,108 +54,97 @@ const TaskDialog = ({ id, data }: { id: string; data: TaskData }) => {
 
     return (
         <Flex>
-            <Dialog.Content onKeyDown={handleKeyDown}>
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        handleSave();
-                    }}
-                >
-                    <Flex justify="between" align="center">
-                        <Dialog.Title className="text-lg font-semibold">
-                            Edit Task
-                        </Dialog.Title>
-                        <Dialog.Close>
-                            <Text className="relative bottom-3 cursor-pointer">
-                                x
-                            </Text>
-                        </Dialog.Close>
-                    </Flex>
-                    <Dialog.Description></Dialog.Description>
-                    <DataList.Root>
-                        <DataList.Item>
-                            <DataList.Label>ID</DataList.Label>
-                            <DataList.Value>{id}</DataList.Value>
-                        </DataList.Item>
-                        <DataList.Item>
-                            <DataList.Label>Task</DataList.Label>
-                            <DataList.Value>
-                                <TextField.Root
-                                    value={label}
-                                    onChange={(e) => setLabel(e.target.value)}
-                                    placeholder="task title"
-                                    className="w-full"
-                                />
-                            </DataList.Value>
-                        </DataList.Item>
-                        <DataList.Item>
-                            <DataList.Label>Start</DataList.Label>
-                            <DataList.Value>
-                                <input
-                                    type="date"
-                                    value={start}
-                                    onChange={(e) => {
-                                        const newStart = e.target.value;
-                                        setStart(newStart);
-                                    }}
-                                    className="border px-2 py-1 rounded border-gray-600"
-                                />
-                            </DataList.Value>
-                        </DataList.Item>
-                        <DataList.Item>
-                            <DataList.Label>Deadline</DataList.Label>
-                            <DataList.Value>
-                                <input
-                                    type="date"
-                                    value={deadline}
-                                    onChange={(e) => {
-                                        const newDeadline = e.target.value;
-                                        if (
-                                            start &&
-                                            new Date(start) > new Date(newDeadline)
-                                        ) {
-                                            alert(
-                                                "The start time cannot be later than the end time"
-                                            );
-                                            return;
-                                        }
-                                        setDeadline(newDeadline);
-                                    }}
-                                    className="border px-2 py-1 rounded border-gray-600"
-                                />
-                            </DataList.Value>
-                        </DataList.Item>
-                        <DataList.Item>
-                            <DataList.Label>Note</DataList.Label>
-                            <DataList.Value>
-                                <TextArea
-                                    value={description}
-                                    onChange={(e) => setDescription(e.target.value)}
-                                    placeholder={"write description here..."}
-                                    className="w-full"
-                                />
-                            </DataList.Value>
-                        </DataList.Item>
-                    </DataList.Root>
-                    <Flex justify="end" gapX="2" className="mt-8">
-                        <TaskStatusUpdateButton id={id} />
-                        <Dialog.Close ref={closeRef}>
-                            <Button
-                                color="blue"
-                                variant="solid"
-                                onClick={handleSave}
-                                loading={loading}
-                                disabled={loading}
-                                className="rounded-full px-4 py-2 flex items-center"
-                            >
-                                <Text size="1">Save</Text>
-                                <Text size="1" className="ml-2 bg-blue-500 px-2 py-0.5 rounded text-white">
-                                    {shortcutLabel}
-                                </Text>
-                            </Button>
-                        </Dialog.Close>
-                    </Flex>
-                </form>
+            <Dialog.Content>
+                <Flex justify="between" align="center">
+                    <Dialog.Title className="text-lg font-semibold">
+                        Edit Task
+                    </Dialog.Title>
+                    <Dialog.Close>
+                        <Text className="relative bottom-3 cursor-pointer">
+                            x
+                        </Text>
+                    </Dialog.Close>
+                </Flex>
+                <Dialog.Description></Dialog.Description>
+                <DataList.Root>
+                    <DataList.Item>
+                        <DataList.Label>ID</DataList.Label>
+                        <DataList.Value>{id}</DataList.Value>
+                    </DataList.Item>
+                    <DataList.Item>
+                        <DataList.Label>Task</DataList.Label>
+                        <DataList.Value>
+                            <TextField.Root
+                                value={label}
+                                onChange={(e) => setLabel(e.target.value)}
+                                placeholder="task title"
+                                className="w-full"
+                            />
+                        </DataList.Value>
+                    </DataList.Item>
+                    <DataList.Item>
+                        <DataList.Label>Start</DataList.Label>
+                        <DataList.Value>
+                            <input
+                                type="date"
+                                value={start}
+                                onChange={(e) => {
+                                    const newStart = e.target.value;
+                                    setStart(newStart);
+                                }}
+                                className="border px-2 py-1 rounded border-gray-600"
+                            />
+                        </DataList.Value>
+                    </DataList.Item>
+                    <DataList.Item>
+                        <DataList.Label>Deadline</DataList.Label>
+                        <DataList.Value>
+                            <input
+                                type="date"
+                                value={deadline}
+                                onChange={(e) => {
+                                    const newDeadline = e.target.value;
+                                    if (
+                                        start &&
+                                        new Date(start) > new Date(newDeadline)
+                                    ) {
+                                        alert(
+                                            "The start time cannot be later than the end time"
+                                        );
+                                        return;
+                                    }
+                                    setDeadline(newDeadline);
+                                }}
+                                className="border px-2 py-1 rounded border-gray-600"
+                            />
+                        </DataList.Value>
+                    </DataList.Item>
+                    <DataList.Item>
+                        <DataList.Label>Note</DataList.Label>
+                        <DataList.Value>
+                            <TextArea
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                                placeholder={"write description here..."}
+                                className="w-full"
+                            />
+                        </DataList.Value>
+                    </DataList.Item>
+                </DataList.Root>
+                <Flex justify="end" gapX="2" className="mt-8">
+                    <TaskStatusUpdateButton id={id} />
+                    <Dialog.Close>
+                        <Button
+                            color="blue"
+                            variant="solid"
+                            onClick={handleSave}
+                            loading={loading}
+                            disabled={loading}
+                        >
+                            <Text size="1">Save</Text>
+                        </Button>
+                    </Dialog.Close>
+                </Flex>
             </Dialog.Content>
         </Flex>
     );
