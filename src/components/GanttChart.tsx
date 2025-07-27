53ddf5146aa18b33d43164393b4e8008ce97f4e6
呂翰昇 <alanlu131089582@gmail.com>
2025-07-27 17:18:15 +0800

    hot fix

--- src/components/GanttChart.tsx
+++ src/components/GanttChart.tsx
@@ -98,15 +98,18 @@ const GanttChart = () => {
                 eventContent={renderEventContent}
                 viewClassNames={"rounded-lg overflow-hidden"}
             />
-            {selectedTaskId && (
-                <Dialog.Root open={dialogOpen} onOpenChange={setDialogOpen}>
-                    <TaskDialog
-                        key={selectedTaskId}
-                        id={selectedTaskId!}
-                        data={tasks.find((t) => t.id === selectedTaskId)!.data}
-                    />
-                </Dialog.Root>
-            )}
+            {selectedTaskId && (() => {
+                const selectedTask = tasks.find((t) => t.id === selectedTaskId);
+                return selectedTask ? (
+                    <Dialog.Root open={dialogOpen} onOpenChange={setDialogOpen}>
+                        <TaskDialog
+                            key={selectedTaskId}
+                            id={selectedTaskId}
+                            data={selectedTask.data}
+                        />
+                    </Dialog.Root>
+                ) : null;
+            })()}
             <StateBar />
         </div>
     );
